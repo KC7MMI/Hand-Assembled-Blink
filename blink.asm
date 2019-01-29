3932cfdb6c89a6fbc779e9204a61dd341ac07991
Benjamin Russell <40191081+KC7MMI@users.noreply.github.com>
2019-01-29 16:50:45 -0700

    Update blink.asm

--- blink.asm
+++ blink.asm
@@ -5,9 +5,9 @@
 .dw 0b1110001000010000	;ldi r17, $20     ;load 1 into 5th bit of r17
 .dw 0b0010111100000001	;mov r16, r17     ;copy r17 to r16
 .dw 0b1011100100000101	;out $05, r16     ;PORTB5 high (LED on)
-.dw 0b1110000100101111	;ldi r18, $1f     ;initialize counter to 31
-.dw 0b1110111110011111	;ldi r25, $ff     ;initialize 16-bit prescaler to 65535
-.dw 0b1110111110001111	;ldi r24, $ff     ;''
+.dw 0b1110001100100010	;ldi r18, $32     ;initialize counter to 50
+.dw 0b1110100110011100	;ldi r25, $9c     ;initialize 16-bit prescaler to 40000 (for counter inc's of 10mS)
+.dw 0b1110010010000000	;ldi r24, $40     ;''
 .dw 0b1001011100000001	;sbiw r25:r24, 1  ;decrement prescaler by 1
 .dw 0b1111011111110001	;brne PC-1        ;loop until prescaler 0
 .dw 0b1001010100101010	;dec r18          ;decrement counter by 1
